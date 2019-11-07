4819260772bb21af7379a35e5aa70921a50a1311
felipe.xavier <felipe.xavier@funceme.br>
2019-11-07 07:58:17 -0300

    Adding bootstrap css and localhost to allowed hosts.

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -27,7 +27,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = ['clients-manager-0800.herokuapp.com']
+ALLOWED_HOSTS = ['clients-manager-0800.herokuapp.com', 'localhost']
 
 
 # Application definition
